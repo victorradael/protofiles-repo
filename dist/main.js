aa26b38416787c84e1355b09b7f37010143cb8fb
Victor Radael <victor.radael@datasprints.com>
2022-03-04 18:26:39 -0300

    FIX: Att Propo Path

--- dist/main.js
+++ dist/main.js
@@ -2,5 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserProtofile = void 0;
 var path_1 = require("path");
-var UserProtofile = (0, path_1.join)(__dirname, "lib", "user", "user.proto");
+var UserProtofile = (0, path_1.join)(__dirname, "..", "protos", "user.proto");
 exports.UserProtofile = UserProtofile;
+console.log(UserProtofile);
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { join } from "path";
 import { UserServiceInterface } from "./lib/user/user_interface";
 
-const UserProtofile = join(__dirname, "lib", "user", "user.proto");
+const UserProtofile = join(__dirname, "..", "protos", "user.proto");
+console.log(UserProtofile);
 
 export { UserProtofile, UserServiceInterface };
